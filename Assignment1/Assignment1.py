de4b49ea2bf1fa8576f821c779de68fd8b8d6d96
WajihaMalik <wajihakanwal2019@gmail.com>
2022-01-01 22:41:36 +0500

    Work Accomplish

---
+++ Assignment1/Assignment1.py
@@ -0,0 +1,31 @@
+'''
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+'''
+ 
+print("Twinkle, twinkle, little star, \n\tHow I wonder what you are! \n\t\tUp above the world so high, \n\t\tLike a diamond in the sky. \nTwinkle, twinkle, little star, \n\tHow I wonder what you are!")
+
+import sys
+print(sys.version)
+
+import datetime
+now = datetime.datetime.now()
+print ("Current date and time : ")
+print (now.strftime("%Y-%m-%d %H:%M:%S"))
+
+from math import pi
+r = float(input ("Input the radius of the circle : "))
+print ("The area of the circle with radius " + str(r) + " is: " + str(pi * r**2))
+
+fname = input("Input your First Name : ")
+lname = input("Input your Last Name : ")
+print ( lname + " " + fname)
+
+num1 = input('Enter first number: ')
+num2 = input('Enter second number: ')
+sum = float(num1) + float(num2)
+print('The sum of {0} and {1} is {2}'.format(num1, num2, sum))
